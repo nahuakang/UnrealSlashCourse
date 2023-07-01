8a3ce5f14196c73598ef02b6342a795e4be7816c
Nahua Kang <kangnahua@gmail.com>
2023-07-01 13:20:39 +0200

    84: Use AnimInstance in C++ for Animation Blueprint for Echo to move between Idle and Run

---
+++ Source/UnrealCourse/Public/Characters/SlashAnimInstance.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashAnimInstance.generated.h"
+
+// Forward Declarations
+class ASlashCharacter;
+class UCharacterMovementComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCOURSE_API USlashAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	ASlashCharacter* SlashCharacter;
+
+	UPROPERTY(BlueprintReadOnly)
+	UCharacterMovementComponent* SlashCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+};
