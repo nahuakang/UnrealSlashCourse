c18abc734ea05eb87ac6dbdfd2495bf93591d409
Nahua Kang <kangnahua@gmail.com>
2023-07-03 19:18:26 +0200

    95: Create the Weapon class from AItem class

--- Source/UnrealCourse/Public/Items/Item.h
+++ Source/UnrealCourse/Public/Items/Item.h
@@ -34,12 +34,13 @@ protected:
 	float TransformedCosin();
 
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
-	                     int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                             UPrimitiveComponent* OtherComp,
+	                             int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 	UFUNCTION()
-	void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	                        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	                                UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 	template <typename T>
 	T Avg(T First, T Second);
